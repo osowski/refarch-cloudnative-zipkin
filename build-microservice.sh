bfa50e78a5a6249243bf6b588cf676bbe33d0deb
edwin van de grift <edwinvandegrift@us.ibm.com>
2016-11-21 14:14:29 -0700

    Added curl as alternative for wget in build

--- build-microservice.sh
+++ build-microservice.sh
@@ -10,8 +10,15 @@ while getopts "d" ARG; do
   esac
 done
 
-wget -O zipkin.jar 'https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec'
-
+if [[ $(which curl) ]]; then
+	curl -L --output zipkin.jar 'https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec'
+elif [[ $(which wget) ]]; then
+	wget -O zipkin.jar 'https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec'
+else
+	echo "Neither wget nor curl is installed. Cannot retrieve zipkin.jar."
+	exit 1
+fi
+	
 if [[ ${DO_DOCKER} == 'yes' ]]; then
   cp zipkin.jar docker/app.jar
   cd docker/
